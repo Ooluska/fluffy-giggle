2dea3a93800fdda1ed208a397a0b6fa4c3397f21
Ooluska <tr52a@mail.ru>
2025-01-24 20:56:19 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const btn0 = document.querySelector('#btn0')
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('#btn2')
+const btn3 = document.querySelector('#btn3')
+const btn4 = document.querySelector('#btn4')
+const btn5 = document.querySelector('#btn5')
+const btn6 = document.querySelector('#btn6')
+const btn7 = document.querySelector('#btn7')
+const btn8 = document.querySelector('#btn8')
+const btn9 = document.querySelector('#btn9')
+const btnMinus = document.querySelector('#btnMinus')
+const btnPlus = document.querySelector('#btnPlus')
+const btnMultiply = document.querySelector('#btnMultiply')
+const btnEquals = document.querySelector('#btnEquals')
+const btnDivide = document.querySelector('#btnDivide')
+
+const result = document.querySelector('#result')
+const listResult = document.querySelector('#listResult')
+const btnList = document.querySelector('#btnList')
+
+const numbers = []
+const resultNumbers =[]
+
+let actionSymbol = 0
+
+
+
+function createNumber(actionSymbol){
+    numbers.push(actionSymbol)
+}
+
+function renderResult(actionSymbol){
+
+
+}
+
+btnList.onclick = function(event){
+   
+    
+  
+    
+}
